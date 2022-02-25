434dc601745f94dab04c7f2f2bb07ce76e02bde2
Pratig Sonar <38097576+pratig-sonar@users.noreply.github.com>
2022-02-26 02:10:19 +0530

    Update Interface1.java

--- Java/Interface/Interface1.java
+++ Java/Interface/Interface1.java
@@ -1,1 +1,17 @@
-
+interface Car{
+    public void sound();
+  }
+  interface Bike{
+    public void sound();
+  }
+  class Interface1 implements Car, Bike{
+    @Override
+    public void sound(){
+      System.out.println("I can hear the sound.");
+    }
+    public static void main(String args[]){
+      Interface1 interface1 = new Interface1();
+      ((Car) interface1).sound();
+      ((Bike) interface1).sound();
+    }
+  }
